c7873fec8cc53044fbd45da4a7a73754ce4ef1dd
Daniel Craig <daniel.craig@montvieux.com>
2022-12-02 16:50:40 +0000

    changed some comments and added doc block

--- common/base.py
+++ common/base.py
@@ -17,11 +17,17 @@ from collections.abc import Callable
 class Base:
     input: list = []
 
-    def __init__(self, input_path: str = None, map_function: Callable[[str], any] = str):
-        # get current day and script directory
+    def __init__(self, input_path: str = None, map_function: Callable[[str], any] = str) -> None:
+        """
+        Initialise the puzzle object, read in the input, clean/cast it and store each line in `self.input`
+        :param input_path: str, relative path from day directory to input file
+        :param map_function: a function to apply to each line of the input file
+        """
+
+        # get current script directory
         script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
 
-        # default to day input file name format
+        # default to input path
         if input_path is None:
             input_path = "input"
 
@@ -30,5 +36,5 @@ class Base:
 
         with open(abs_input_path, "r") as f:
             # read all lines in passing it through a cast/clean/format function e.g.
-            # lambda x:x.split(" ")   or just int
+            # `lambda x:x.split(" ")`   or just `int`
             self.input = map(map_function, f.readlines())
