d945b4ef7dc3d2e9a156167b3bb2b6c87df13da9
Daniel Craig <daniel.craig@montvieux.com>
2022-12-11 22:25:50 +0000

    added comments for day8

--- day8/part1.py
+++ day8/part1.py
@@ -10,17 +10,28 @@ __version__ = "1.0.0"
 __maintainer__ = "Daniel Craig"
 __status__ = "Development"
 
-from copy import copy
-
 import numpy as np
-
-from common.base import Base
 from colorama import Fore, Style
 
+from common.base import Base
+
+
 class Puzzle(Base):
-    def is_visible_from_edge(self, line_of_trees, visible):
-        for index in range(1, len(line_of_trees)-1):
+    def is_visible_from_edge(self, line_of_trees: np.ndarray, visible: np.ndarray):
+        """
+        Loop through the provided line of trees (could be row could be column)
+        and determine if it's visible
+
+        :param line_of_trees: ndarray a slice of the input array containing heights
+        :param visible: ndarray the corresponding visibility slice to record if it is visible from an edge
+        :return:
+        """
+        for index in range(1, len(line_of_trees) - 1):
+            # get the height of the current tree
             height = line_of_trees[index]
+
+            # we default to visible as it's easier to switch it off when you get to
+            # a taller tree
             visible_from_left = True
             visible_from_right = True
 
@@ -31,7 +42,7 @@ class Puzzle(Base):
                     break
 
             # check right
-            for i in range(len(line_of_trees)-1, index, -1):
+            for i in range(len(line_of_trees) - 1, index, -1):
                 if line_of_trees[i] >= height:
                     visible_from_right = False
                     break
@@ -41,39 +52,44 @@ class Puzzle(Base):
     def print_forest(self, trees, visible):
         for i, row in enumerate(trees):
             for j, col in enumerate(row):
-                print((Fore.GREEN if visible[i,j] else Fore.BLACK) + str(col), end="")
+                print((Fore.GREEN if visible[i, j] else Fore.BLACK) + str(col), end="")
             print()
         print(Style.RESET_ALL)
 
     def solution(self):
+        # numpy the input so that we can nicely slice it in two dimensions
         trees = np.array(list(self.input), dtype=np.int32)
+
+        # calc overall dimensions of input grid
         width = len(trees[0])
         height = len(trees)
 
-        false_col = [False] * width
-        visible = []
-        for _ in range(height):
-            visible.append(copy(false_col))
-
+        # create a duplicate of the input grid containing booleans
+        # to signify if a tree is visible from an endge
+        visible = [[False] * width for _ in range(height)]
         visible = np.array(visible, dtype=bool)
-        visible[0,:] = True
-        visible[len(visible)-1,:] = True
-        visible[:,0] = True
-        visible[:,len(visible[0])-1] = True
+
+        # set the four edges to visible True
+        visible[0, :] = True
+        visible[len(visible) - 1, :] = True
+        visible[:, 0] = True
+        visible[:, len(visible[0]) - 1] = True
 
         # move down rows
-        for i in range(1, len(trees)-1):
+        for i in range(1, len(trees) - 1):
             self.is_visible_from_edge(trees[i], visible[i])
 
         # move along columns
-        for i in range(1, height-1):
-            self.is_visible_from_edge(trees[:,i], visible[:,i])
+        for i in range(1, height - 1):
+            self.is_visible_from_edge(trees[:, i], visible[:, i])
 
+        # for debug print out the input with some colouring
         self.print_forest(trees, visible)
 
-
+        # sum all of the visible trees True == 1
         return np.sum(visible)
 
+
 if __name__ == "__main__":
-    puzzle = Puzzle(map_function=lambda x:[int(y) for y in str(x).strip()])
+    puzzle = Puzzle(map_function=lambda x: [int(y) for y in str(x).strip()])
     print(puzzle.solution())
--- day8/part2.py
+++ day8/part2.py
@@ -10,48 +10,54 @@ __version__ = "1.0.0"
 __maintainer__ = "Daniel Craig"
 __status__ = "Development"
 
-from copy import copy
-
 import numpy as np
 
 from common.base import Base
 
+
 class Puzzle(Base):
-    def visible_distance(self, line_of_trees, visible):
+    def visible_distance(self, line_of_trees: np.ndarray, visible: np.ndarray):
         for index in range(0, len(line_of_trees)):
+            # get the height of the current tree
             height = line_of_trees[index]
 
             left_count = 0
             right_count = 0
 
             # check left
-            for i in range(index-1, -1, -1):
+            for i in range(index - 1, -1, -1):
+                # total up the number of visible trees
+                # until we get to a tree equal or taller
                 left_count += 1
                 if line_of_trees[i] >= height:
                     break
 
             # check right
-            for i in range(index+1, len(line_of_trees)):
+            for i in range(index + 1, len(line_of_trees)):
+                # total up the number of visible trees
+                # until we get to a tree equal or taller
                 right_count += 1
                 if line_of_trees[i] >= height:
                     break
 
+            # either set or increase the visible tree count
             if visible[index] == 0:
                 visible[index] = left_count * right_count
             else:
                 visible[index] *= left_count * right_count
 
     def solution(self):
+        # numpy the input so that we can nicely slice it in two dimensions
         trees = np.array(list(self.input), dtype=np.int32)
+
+        # calc overall dimensions of input grid
         width = len(trees[0])
         height = len(trees)
 
-        false_col = [0] * width
-        visible = []
-        for _ in range(height):
-            visible.append(copy(false_col))
-
-        visible = np.array(visible, dtype=np.int32)
+        # create a duplicate of the input grid containing booleans
+        # to signify if a tree is visible from an endge
+        visible = [[0] * width for _ in range(height)]
+        visible = np.array(visible, dtype=int)
 
         # move down rows
         for i in range(0, height):
@@ -59,10 +65,12 @@ class Puzzle(Base):
 
         # move along columns
         for i in range(0, width):
-            self.visible_distance(trees[:,i], visible[:,i])
+            self.visible_distance(trees[:, i], visible[:, i])
 
+        # get the largest visible distance
         return np.max(visible)
 
+
 if __name__ == "__main__":
-    puzzle = Puzzle(map_function=lambda x:[int(y) for y in str(x).strip()])
+    puzzle = Puzzle(map_function=lambda x: [int(y) for y in str(x).strip()])
     print(puzzle.solution())
