33b56f6a172ae994e4dad920a624074504c4af80
Daniel Craig <daniel.craig@montvieux.com>
2023-11-28 12:25:29 +0000

    added day template part2

---
+++ day-template/part2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+    A solution to the relevant day's puzzle on https://adventofcode.com/2022
+"""
+
+__author__ = "Daniel Craig"
+__copyright__ = "Copyright 2022"
+__version__ = "1.0.0"
+__maintainer__ = "Daniel Craig"
+__status__ = "Development"
+
+from common.base import Base
+
+
+class Puzzle(Base):
+    def solution(self):
+        return self.input
+
+
+if __name__ == "__main__":
+    puzzle = Puzzle()
+    print(puzzle.solution())
